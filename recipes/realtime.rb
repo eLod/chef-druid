3efc77da2ac43eca2d7f74653f163ecbb82fc7b0
PoTa <pota@mosfet.hu>
2015-08-25 15:41:30 +0200

    Adding spec file for realtime node.

--- recipes/realtime.rb
+++ recipes/realtime.rb
@@ -2,6 +2,15 @@
 #
 # Licensed under Apache 2.0 - see the LICENSE file
 
+node.default[:druid][:realtime][:properties]['druid.realtime.specFile'] = ::File.join node[:druid][:config_dir], 'realtime', 'spec.json'
+
 druid File.basename(__FILE__, ".rb") do
   action :install
+end
+
+template node[:druid][:realtime][:properties]['druid.realtime.specFile'] do
+  source 'realtime.spec.erb'
+  variables spec: node[:druid][:realtime][:spec]
+  owner node[:druid][:user]
+  group node[:druid][:group]
 end
